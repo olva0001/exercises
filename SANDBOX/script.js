0635681c9def0ea88e4f56ea597018d14f8a742b
Oliver Valter <olva0001@kea.stud.dk>
2025-02-04 08:31:01 +0100

    exercises

---
+++ SANDBOX/script.js
@@ -0,0 +1,30 @@
+// let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+
+// console.log(letters[4])
+
+
+// let newarr = letters;
+// newarr[4] = '*';
+
+// console.log(letters[4]);
+
+// console.log(letters);
+
+
+const people = ['harry', 'ron', 'hermione'];
+
+let result;
+
+// result = people.push("Draco");
+// result = people.pop();
+// result = people.push("Neville");
+// result = people.push("Luna");
+// result = people.slice(0, 3);
+// result = people.splice(1, 0, 'Cho');
+// people[1] = 'Ginny';
+// result = people.push('Fred', 'George');
+// result = people.indexOf('Fred');
+// result = people.splice(result, 1);
+
+console.log(result);
+console.log(people);
