b97fb452b37da0e81e03b9c396072e6155639355
Oliver Valter <olva0001@kea.stud.dk>
2025-02-06 14:39:14 +0100

    Language switcher øvelse

---
+++ language_switcher/js.js
@@ -0,0 +1,17 @@
+"use strict";
+const texts = {
+  de: {
+    texts: [
+      { text: "Das Bot", location: ".header" },
+      { text: "Das Ro-Bot", location: ".footer" },
+    ],
+  },
+  da: {
+    texts: [
+      { text: "Båden", location: ".header" },
+      { text: "Robotten", location: ".footer" },
+    ],
+  },
+};
+const locale = "da";
+
