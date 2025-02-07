c897770839fee6d2678409a6b53266c7b57ed6ad
Oliver Valter <olva0001@kea.stud.dk>
2025-02-07 15:01:42 +0100

    name parts

---
+++ Name_Parts/script.js
@@ -0,0 +1,11 @@
+
+
+const Navn = "Oliver Freisleben Valter";
+
+const NavneDele = Navn.split(" ");
+
+console.log(NavneDele);
+
+const firstName = NavneDele[0];
+const middleName = NavneDele[1];
+const lastName = NavneDele[2];
