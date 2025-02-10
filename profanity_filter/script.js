8539b426add9f67e488eba5e6ec97fbbe46af59e
Oliver Valter <olva0001@kea.stud.dk>
2025-02-10 14:41:55 +0100

    Profanity filter

---
+++ profanity_filter/script.js
@@ -0,0 +1,31 @@
+const curseWords = [  {    bad: "var",    good: "const",  },  {    bad: "float",    good: "grid",  },  {    bad: "marquee",    good: "just don't",  },];
+
+document.getElementById("SFW").addEventListener("click", MakeTextSafeForWork);
+
+let wordsReplaced = false;
+
+document.getElementById("SFW").addEventListener("click", MakeTextSafeForWork);
+
+function MakeTextSafeForWork() {
+    let textElement = document.getElementById("text-content");
+    let originalText = textElement.textContent;
+    let modifiedText = originalText;
+
+    if (!wordsReplaced) {
+        curseWords.forEach(({ bad, good }) => {
+            const regex = new RegExp(`\\b${bad}\\b`, "g");
+            modifiedText = modifiedText.replace(regex, `<span class="styled-good-word">${good}</span>`);
+        });
+
+        if (modifiedText !== originalText) {
+            textElement.innerHTML = modifiedText;
+            wordsReplaced = true;
+        }
+    } else {
+        openDialog();
+    }
+}
+
+function openDialog() {
+    document.getElementById("confirmationDialog").showModal();
+}
