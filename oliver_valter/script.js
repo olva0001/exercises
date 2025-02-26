58ea68093edf75f637fe45c5af91e8ac191c9ea7
Oliver Valter <olva0001@kea.stud.dk>
2025-02-26 17:35:37 +0100

    kkkkkkkkk

--- oliver_valter/script.js
+++ oliver_valter/script.js
@@ -1,3 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".menu_container .button");
+    const textFields = document.querySelectorAll(".text_field div");
+    
+    buttons.forEach((button, index) => {
+        button.addEventListener("click", function () {
+            // Hide all text fields
+            textFields.forEach(field => field.style.display = "none");
+            
+            // Show the corresponding text field
+            textFields[index].style.display = "block";
+        });
+    });
+    
+    // Hide all text fields initially
+    textFields.forEach(field => field.style.display = "none");
+});
+
 var button = document.getElementsByClassName("button");
 
 for (var i = 0; i < button.length; i++) {
